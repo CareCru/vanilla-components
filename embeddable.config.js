6c0c081d919ddf130614e3acc391a1840867e474
Dennis Mah <dennis.mah@carecru.com>
2024-11-06 16:50:51 -0700

    Adding the cube and needed base URLs to run the POC.

--- embeddable.config.js
+++ embeddable.config.js
@@ -4,8 +4,8 @@ import react from '@embeddable.com/sdk-react';
 export default defineConfig({
   plugins: [react],
 
-  // previewBaseUrl: 'https://app.eu.embeddable.com',
-  // pushBaseUrl: 'https://api.eu.embeddable.com',
+  previewBaseUrl: 'https://app.us.embeddable.com',
+  pushBaseUrl: 'https://api.us.embeddable.com',
   // audienceUrl: 'https://auth.eu.embeddable.com',
   // authDomain: 'auth.eu.embeddable.com',
   // authClientId: '6OGPwIQsVmtrBKhNrwAaXhz4ePb0kBGV',
